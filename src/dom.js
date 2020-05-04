a50dd627d5b8c7672f00b4f916124fec8611a008
Messias Dias <messiasdias.ti@gmail.com>
2020-05-04 11:12:06 -0300

    04-05-2020 11:12

--- src/dom.js
+++ src/dom.js
@@ -8,28 +8,43 @@ let breakpoints =  {
 
 
 let dom = { 
-
-    image : function (){
-        if( window.innerWidth >= breakpoints.medium ){
-            $('.left>.img-content').html( $('.right>.img-content').html() )
-        }
+    // < 
+    scroolIsSml: function($num){
+        return ($(document).scrollTop() < $num);
+    },
+    // >=
+    scroolIsBig: function($num){
+        return ($(document).scrollTop() >= $num);
     },
 
+    isSmall: function(){
+        return window.innerWidth < breakpoints.medium
+    },
+
+    isMedium: function(){
+        return (window.innerWidth >= breakpoints.medium) && (window.innerWidth < breakpoints.large)
+    },
+
+    isLarge: function(){
+        return window.innerWidth >= breakpoints.large
+    },
+
+    image : function (){
+        if( dom.isMedium() | dom.isLarge() ){
+            $('.left>.img-content').html( $('.right>.img-content').html() )
+        }
+
+        this.avatar()
+    },
 
     avatar: function  () {
-
-        if( window.innerWidth <= breakpoints.medium ) { 
-
-            if(  ( $(document).scrollTop() > 200 )  &&
-                ( $(document).scrollTop() < ($('.right').height() ) )  ){
-                
+        if( dom.isSmall() ) { 
+            if( dom.scroolIsBig(200) && dom.scroolIsSml($('.right').height()) ){
                 $('#avatar').html( $('.right>.img-content').html() )
                 $('#avatar').show('fast')
-
             }else{
                 $('#avatar').hide()
             }
-
         }else{
             $('#avatar').hide()
         }
@@ -37,31 +52,69 @@ let dom = {
 
 
     btn: function(){
-        if($(document).scrollTop() >= ($('.right').height() - 100)  ){
-            $('.btn').addClass('btn-relative')
-        }else{
-            $('.btn').removeClass('btn-relative')
+
+       if( dom.isSmall()  ){
+
+            if( dom.scroolIsBig( 800 ) | dom.scroolIsBig( $('.right').height() )  ){
+                $('.btn').removeClass('btn-fixed-lg')
+                $('.btn').removeClass('btn-fixed-md')
+                $('.btn').addClass('btn-relative')
+            }else{
+                $('.btn').removeClass('btn-fixed-lg')
+                $('.btn').removeClass('btn-relative')
+                $('.btn').addClass('btn-fixed-md')
+            }
         }
+        
+        if ( dom.isMedium() ){
+            $('.btn').addClass('btn-fixed-lg')
+
+            if( dom.scroolIsSml( 200 )  ){
+                $('.btn').removeClass('btn-relative')
+                $('.btn').removeClass('btn-fixed-md')
+            }
+        } 
+
+
+        if( dom.isLarge() ){
+            $('.btn').removeClass('btn-fixed-md')
+            $('.btn').removeClass('btn-relative')
+            $('.btn').addClass('btn-fixed-lg')
+        }
+
     }
+
+
 }   
 
 
 //On Ready
-$(document).ready(()=>{
-
+$(document).ready( function() {
     setTimeout( function(){
        $(this).scrollTop(0)
     }, 10)
 
+    dom.avatar()
+    dom.btn()
     dom.image()
 })
 
 //On Scroll
 $(document).scroll( function(){
-    dom.avatar();
+    dom.avatar()
     dom.btn()
-    console.log( $('.right').height() , $(document).scrollTop() )
+    dom.image()
 })
+
+//On Resize
+$(window).resize( function(){
+    dom.avatar()
+    dom.btn()
+    dom.image()
+})
+
+
+
 
 //export
 export default { dom:dom, breakpoints: breakpoints}
