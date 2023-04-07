222d462e24c235fe8757990b563f96c3da87acf5
Messias Dias <messiasdias.ti@gmail.com>
2023-04-07 16:55:05 -0300

    Adiciona experiencia CG Contadores e ajusta store.js

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,10 +2,10 @@ import "./sass/main.scss"
 import Vue from "vue"
 import Vuex from "vuex"
 import VueMeta from 'vue-meta'
-import FontAwesomeIcon  from "./icons"
+import FontAwesomeIcon  from "./dom/icons"
 import Curriculum from "./components/Curriculum.vue"
 import Store from "./store"
-import "./dom"
+import "./dom/dom"
 
 Vue.component('fontawesome',FontAwesomeIcon)
 Vue.use(VueMeta)
---
+++ ui/src/services/experiencias.js
@@ -0,0 +1,59 @@
+export default [
+    /*
+    {
+        empresa: 'Vertice Digital',
+        cargo: 'Desenvolvedor FullStack JR',
+        periodo: 'Jan/2021 - Abr/2021',
+        descricao: [
+          'Desenvolvimento de interfaces web com Vue.js',
+          'Manuteção em aplicações híbridas Laravel e Slim Framework (PHP)'
+        ]
+    },
+    */
+    {
+        empresa: 'CG Profissionais em RH LTDA',
+        cargo: 'Desenvolvedor FullStack JR',
+        periodo: 'Jan/2023 - Mai/2023',
+        descricao: [
+          'Manutenção de interfaces web com Angular',
+          'Manuteção em aplicações híbridas Laravel e Codeigniter'
+        ]
+    },
+    {
+        empresa: 'KEVI | Retenção de Cliente',
+        cargo: 'Desenvolvedor FullStack JR',
+        periodo: 'Mai/2021 - Jan/2023',
+        descricao: [
+          'Desenvolvimento de interfaces web com Vue.js',
+          'Manuteção em aplicações híbridas Slim Framework e Proprietário',
+          'Testes JS com Jest e PHP com Php Unit'
+        ]
+    },
+    {
+      empresa: 'Vértice Digital',
+      cargo: 'Desenvolvedor FullStack JR',
+      periodo: 'Jan/2021 - Abr/2021',
+      descricao: [
+        'Desenvolvimento de interfaces web com Vue.js',
+        'Manuteção em aplicações híbridas Laravel e Slim Framework'
+      ]
+    },
+    {
+      empresa: 'FUTURA DES. DE PROGRAMAS EIRELI',
+      cargo: 'Desenvolvedor Front-End JR',
+      periodo: 'Jun/2020 - Jan/2021',
+      descricao: [
+        'Desenvolvimento de interfaces web com Vue.js e Materialize'
+      ]
+    },
+    {
+      empresa: 'SETEV',
+      cargo: 'Analista de Suporte JR',
+      periodo: 'Fev/2018 - Set/2019',
+      descricao: [
+        'Administração de Servidores Linux Ubuntu/Debian',
+        'NovoSGA - Implantação do Sistema de Gerenciamento de Atendimento',
+        'NovoSGA - Desenvolvimento Front-End de Painel de Atendimento exclusivo'
+      ]
+    }
+]
