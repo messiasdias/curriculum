8e2cbe16bdc92cc024ae47cc7620c2d3f09bae12
Messias Dias <messiasdias.ti@gmail.com>
2024-09-25 13:41:09 -0300

    Atualiza mensagem do popup

--- ui/src/dom/icons.js
+++ ui/src/dom/icons.js
@@ -1,47 +1,47 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { 
-    faTimes, 
-    faEnvelope,
-    faPrint,
-    faAddressBook,
-    faNetworkWired,
-    faCodeBranch,
-    faInfo,
-    faGraduationCap,
-    faBriefcase,
-    faDollarSign,
-    faBullseye,
-    faHeart,
-    faTasks,
-    faMobileAlt,
+import {
     faAt,
     faLink,
-} 
-from '@fortawesome/free-solid-svg-icons'
+    faInfo,
+    faTimes,
+    faPrint,
+    faHeart,
+    faTasks,
+    faComment,
+    faEnvelope,
+    faBullseye,
+    faMobileAlt,
+    faBriefcase,
+    faCodeBranch,
+    faDollarSign,
+    faAddressBook,
+    faNetworkWired,
+    faGraduationCap,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(
     fab,
-    faTimes,
-    faEnvelope,
-    faPrint,
-    faAddressBook,
-    faNetworkWired,
-    faCodeBranch,
-    faInfo,
-    faGraduationCap,
-    faBriefcase,
-    faDollarSign,
-    faBullseye,
-    faHeart,
-    faTasks,
-    faMobileAlt,
     faAt,
     faLink,
+    faInfo,
+    faTimes,
+    faPrint,
+    faHeart,
+    faTasks,
+    faComment,
+    faEnvelope,
+    faBullseye,
+    faMobileAlt,
+    faBriefcase,
+    faCodeBranch,
+    faDollarSign,
+    faAddressBook,
+    faNetworkWired,
+    faGraduationCap,
 )
-
 
 export default FontAwesomeIcon
